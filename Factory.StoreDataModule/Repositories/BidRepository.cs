1836ac5ba0db7845bbe17ff544c0d214e0d65818
Andrei Kostyrin <ka@ipositron.ru>
2015-03-20 11:08:05 +0200

    BidRepository

---
+++ Factory.StoreDataModule/Repositories/BidRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Factory.StoreDomainModule.Entities;
+using Repository.Pattern.Repositories;
+
+namespace Factory.StoreDataModule.Repositories
+{
+    public static class BidRepository
+    {
+        public static SerialBid GetBidById(this IRepository<SerialBid> repository, int bidId)
+        {
+            return repository.Queryable().FirstOrDefault(s => s.Id == bidId);
+        }
+        public static IEnumerable<SerialBid> GetBids(this IRepository<SerialBid> repository)
+        {
+            return repository.Queryable();
+        }
+    }
+}
